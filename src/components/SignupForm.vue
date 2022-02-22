c459aa96535706c1183aec9f35141d1e3bb6a9aa
junyoung4213 <junyoung4213@gmail.com>
2022-02-23 01:42:38 +0900

    회원 가입 이후의 동작 구현

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,7 +12,8 @@
       <label for="nickname">nickname: </label>
       <input id="nickname" type="text" v-model="nickname" />
     </div>
-    <button type="submit">login</button>
+    <button type="submit">회원 가입</button>
+    <p>{{ logMessage }}</p>
   </form>
 </template>
 
@@ -22,20 +23,30 @@ import { registerUser } from '@/api/index';
 export default {
   data() {
     return {
+      // form values
       username: '',
       password: '',
       nickname: '',
+      // log
+      logMessage: '',
     };
   },
   methods: {
-    submitForm() {
-      console.log('폼 제출');
+    async submitForm() {
       const userData = {
         username: this.username,
         password: this.password,
         nickname: this.nickname,
       };
-      registerUser(userData);
+      const { data } = await registerUser(userData);
+      console.log(data.username);
+      this.logMessage = `${data.username} 님이 가입되었습니다`;
+      this.initForm();
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+      this.nickname = '';
     },
   },
 };
