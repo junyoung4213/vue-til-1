75790cc775925d97c7492f3f7cbb990f26430f14
junyoung4213 <junyoung4213@gmail.com>
2022-02-24 16:18:37 +0900

    로그인 폼 데이터 바인딩과 이벤트 연결

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,19 +1,31 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="">id: </label>
-      <input type="text" id="username" />
+      <input type="text" id="username" v-model="username" />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input type="text" id="password" />
+      <input type="text" id="password" v-model="password" />
     </div>
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      axios.post();
+    },
+  },
+};
 </script>
 
 <style></style>
