4c17a9d53401b1976ec756226e4f5e47a1a6d931
junyoung4213 <junyoung4213@gmail.com>
2022-02-24 16:28:43 +0900

    로그인 기능 구현

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -9,20 +9,36 @@
       <input type="text" id="password" v-model="password" />
     </div>
     <button type="submit">로그인</button>
+    <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
+import { loginUser } from '@/api/index';
+
 export default {
   data() {
     return {
+      // form values
       username: '',
       password: '',
+      // log message
+      logMessage: '',
     };
   },
   methods: {
-    submitForm() {
-      axios.post();
+    async submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+      const { data } = await loginUser(userData);
+      this.logMessage = `${data.user.username} 님 환영합니다`;
+      this.initForm();
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
     },
   },
 };
