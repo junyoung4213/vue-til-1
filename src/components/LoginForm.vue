ff4af5de70993c410b14cf8accb6581fd1f97896
junyoung4213 <junyoung4213@gmail.com>
2022-02-24 16:17:11 +0900

    로그인 폼 컴포넌트 생성 및 마크업

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div>
+      <label for="">id: </label>
+      <input type="text" id="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="text" id="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
